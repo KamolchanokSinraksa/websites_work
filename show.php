5ee0adf27c7e1b64f55c9ab790577f36b4bbea99
KamolchanokSinraksa <73693775+KamolchanokSinraksa@users.noreply.github.com>
2020-11-13 17:30:32 +0700

    Update show.php

--- show.php
+++ show.php
@@ -5,7 +5,7 @@
 <body>
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'kamolchanok-server.mysql.database.azure.com', 'Kamolchanok@kamolchanok-server', 'Kmitl080445', 'kamolchanok-server', 3306);
+mysqli_real_connect($conn, 'kamolchanok-server.mysql.database.azure.com', 'Kamolchanok@kamolchanok-server', 'Kmitl080445', 'ITFLab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
