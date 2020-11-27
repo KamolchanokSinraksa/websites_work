ab522afec1eeced3dbba2de5520b1240996b193e
KamolchanokSinraksa <73693775+KamolchanokSinraksa@users.noreply.github.com>
2020-11-27 17:53:35 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -7,7 +7,7 @@ if (mysqli_connect_errno($conn))
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
-$sql = 'EDIT FROM bmi WHERE ID = '.$_GET['ID'].'';
+$sql = 'EDIT FROM bmi WHERE ID = '.$_GET['id'].'';
 
 if(mysqli_query($conn, $sql)) {
     echo "edit COMPLETED";
